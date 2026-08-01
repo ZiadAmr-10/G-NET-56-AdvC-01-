a6b00caa1dcdc37c682b28f0d7daecbd27cb6290
ZiadAmr-10 <ziadddddamr@gmail.com>
2026-08-01 16:39:01 +0300

    Question01

--- Assignment01/Program.cs
+++ Assignment01/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Question01
+            /*
+            Generic class uses one or more type parameters that
+            are replaced with actual types when an object is created 
+            use it to provide type safty,no boxing and unboxing 
+            Better performance ,reduce duplicate code
+             */
+            #endregion
         }
     }
 }
